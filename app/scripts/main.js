6a0efa43941b2c03536b86c8466c5780bd738650
Yura Bazaliy <yurchykxxx@gmail.com>
2016-01-25 16:54:31 +0200

    Додаю js для datepicker та input mask

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,36 @@
-$('#language').selectpicker({
-  width: 'fit'
-});
 
 $(document).ready(function(){
 
+  $('#language').selectpicker({
+
+    width: 'fit'
+
+  });
+
   $('#mobile').inputmask('+99 9(99) 999-99-99');
+  $('#email').inputmask({
+    mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+    greedy: false,
+    onBeforePaste: function (pastedValue, opts) {
+      pastedValue = pastedValue.toLowerCase();
+      return pastedValue.replace("mailto:", "");
+    },
+    definitions: {
+      '*': {
+        validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+        cardinality: 1,
+        casing: "lower"
+      }
+    }
+  });
+
+  var lang = $('html').attr('lang');
+
+  $('.input-daterange').datepicker({
+
+    language: lang,
+    orientation: 'right bottom'
+
+  });
 
 });
