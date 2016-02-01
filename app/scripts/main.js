4464a811d891738bb6054258fe1084f36d0ba76b
Bazaliy Yura <yurchykxxx@gmail.com>
2016-02-01 11:57:18 +0200

    Пишу js для замовлення

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
 
+  // Laguage dropdown
   $('#language').selectpicker({
-
     width: 'fit'
-
   });
 
+  // Input mask
   $('#mobile').inputmask('+99 9(99) 999-99-99');
   $('#email').inputmask({
     mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
@@ -24,12 +24,28 @@ $(document).ready(function(){
     }
   });
 
+  // Date range
   var lang = $('html').attr('lang');
 
   $('.input-daterange').datepicker({
 
     language: lang,
     orientation: 'right bottom'
+
+  });
+
+  // Booking modal
+  $('.btn--booking').on('click', function(event){
+
+    event.preventDefault();
+    var tour = this.closest('.tour');
+    var src = $(tour).find('.tour__img').attr('src');
+    var tourName = $(tour).find('.tour__name a').text();
+    var price = $(tour).find('.tour__price').text();
+    var stars = $(tour).find('.tour__prestige');
+
+    console.log(src);
+    $('#reserve-modal').modal();
 
   });
 
